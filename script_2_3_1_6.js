255ad0c7a9860f720cf73b3f0350b245697a8b1c
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-10-20 23:06:43 +0900

    JSXを使用したDOM定義（入れ子）

---
+++ script_2_3_1_6.js
@@ -0,0 +1,14 @@
+// JSXを使用したDOM定義（入れ子）
+
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <section className="box">
+            <h1>Message</h1>
+            <p className="hello">Hello,world</p>
+        </section>
+    )
+};
+
+export default Hello;
